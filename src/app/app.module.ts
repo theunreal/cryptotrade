a2b212b988cbbeb7c5365afde62ffe15a60b0e69
Eliran Elnasi (eelnasi) <eelnasi@cisco.com>
2018-01-09 10:04:52 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDlJyFePoqH4SbCLNnGgwNiNowlg60oHQM",
+    authDomain: "nodemucu-10c05.firebaseapp.com",
+    databaseURL: "https://nodemucu-10c05.firebaseio.com",
+    projectId: "nodemucu-10c05",
+    storageBucket: "nodemucu-10c05.appspot.com",
+    messagingSenderId: "836455838403"
+  }
+};
+
 @NgModule({
   declarations: [
     MyApp,
